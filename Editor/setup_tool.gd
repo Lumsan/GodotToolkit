b7d336cbb5bbff67b3aa10e80f2da12e6ab6b557
Lumsan <sant8ri@gmail.com>
2026-03-11 20:06:07 +0200

    dth

--- Editor/setup_tool.gd
+++
@@ -1,8 +0,0 @@
-@tool
-extends EditorScript
-
-func _run() -> void:
-	var window_script := load("res://Toolkit/GodotToolkit/Editor/setup_window.gd")
-	var window: Window = window_script.new()
-	EditorInterface.get_base_control().add_child(window)
-	window.popup_centered(Vector2i(520, 480))
